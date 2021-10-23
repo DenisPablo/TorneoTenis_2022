7877476b3ae171c95e0a15718e47bba9adfc0889
AuriIris <auriol.romina@gmail.com>
2021-10-23 10:37:28 -0300

    Initial commit

---
+++ src/Modelo/Conexion.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author martinbordon
+ */
+public class Conexion {
+   
+    private String url="jdbc:mysql://localhost/universidad";
+    private String usuario="root";
+    private String password="";
+    private Connection conexion;
+    
+    public Conexion() throws ClassNotFoundException{
+            Class.forName("org.mariadb.jdbc.Driver");
+        
+    }
+    
+    public Conexion(String url, String usuario, String password) throws ClassNotFoundException {
+        this.url = url;
+        this.usuario = usuario;
+        this.password = password;
+
+        //Cargamos las clases de mariadb que implementan JDBC
+        Class.forName("org.mariadb.jdbc.Driver");
+
+    }
+    
+    public Connection getConexion() throws SQLException{
+        if(conexion == null){
+                    // Setup the connection with the DB
+            conexion = DriverManager
+                .getConnection(url + "?useLegacyDatetimeCode=false&serverTimezone=UTC"
+                        + "&user=" + usuario + "&password=" + password);
+        }
+        return conexion;
+    }
+    
+}
