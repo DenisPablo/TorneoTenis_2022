15078519fadc4df71e917cc941f9f4a401e324fe
AuriIris <auriol.romina@gmail.com>
2021-11-04 12:12:36 -0300

    bd Definitiva

--- TorneoTenis.sql
+++ TorneoTenis.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 01-11-2021 a las 15:24:49
+-- Servidor: localhost
+-- Tiempo de generación: 04-11-2021 a las 16:06:49
 -- Versión del servidor: 10.4.21-MariaDB
--- Versión de PHP: 8.0.11
+-- Versión de PHP: 7.4.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,8 +18,65 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `torneo tenis`
+-- Base de datos: `Torneo`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `encuentro`
+--
+
+CREATE TABLE `encuentro` (
+  `idEncuentro` int(11) NOT NULL,
+  `jugador1` int(11) NOT NULL,
+  `jugador2` int(11) NOT NULL,
+  `fechaEncuentro` date NOT NULL,
+  `resultado` varchar(30) NOT NULL,
+  `jugadorGanador` int(11) DEFAULT NULL,
+  `estado` varchar(30) NOT NULL,
+  `idEstadio` int(11) NOT NULL,
+  `activo` tinyint(1) NOT NULL,
+  `idTorneo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estadio`
+--
+
+CREATE TABLE `estadio` (
+  `idEstadio` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `ciudad` varchar(30) NOT NULL,
+  `categoria` varchar(30) NOT NULL,
+  `activo` tinyint(1) NOT NULL,
+  `direComercial` varchar(80) NOT NULL,
+  `dimension` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `jugador`
+--
+
+CREATE TABLE `jugador` (
+  `idJugador` int(11) NOT NULL,
+  `nombre` varchar(40) NOT NULL,
+  `dni` int(11) NOT NULL,
+  `fechaNac` date NOT NULL,
+  `altura` double NOT NULL,
+  `peso` double NOT NULL,
+  `estilo` varchar(40) NOT NULL,
+  `manoHabil` tinyint(1) NOT NULL,
+  `torneosGanados` int(11) NOT NULL,
+  `ranking` int(11) NOT NULL,
+  `idPatrocinadores` int(11) NOT NULL,
+  `puntaje` int(11) NOT NULL,
+  `activo` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -29,7 +86,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `patrocinio` (
   `idPatrocinio` int(11) NOT NULL,
-  `idPatrociador` int(11) NOT NULL,
+  `idSponsor` int(11) NOT NULL,
   `idJugador` int(11) NOT NULL,
   `fechIniContrato` date NOT NULL,
   `fechFinContrato` date NOT NULL,
@@ -37,28 +94,133 @@ CREATE TABLE `patrocinio` (
   `Indumentaria` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sponsor`
+--
+
+CREATE TABLE `sponsor` (
+  `idSponsor` int(11) NOT NULL,
+  `marca` varchar(40) NOT NULL,
+  `activo` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `torneo`
+--
+
+CREATE TABLE `torneo` (
+  `idTorneo` int(11) NOT NULL,
+  `nombre` varchar(40) NOT NULL,
+  `fechaNacInicio` date NOT NULL,
+  `fechaNacFinal` date NOT NULL,
+  `activo` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `encuentro`
+--
+ALTER TABLE `encuentro`
+  ADD PRIMARY KEY (`idEncuentro`),
+  ADD KEY `j1` (`jugador1`),
+  ADD KEY `j2` (`jugador2`),
+  ADD KEY `ganador2` (`jugadorGanador`),
+  ADD KEY `estadio` (`idEstadio`);
+
+--
+-- Indices de la tabla `estadio`
+--
+ALTER TABLE `estadio`
+  ADD PRIMARY KEY (`idEstadio`);
+
+--
+-- Indices de la tabla `jugador`
+--
+ALTER TABLE `jugador`
+  ADD PRIMARY KEY (`idJugador`),
+  ADD KEY `Sponsor` (`idPatrocinadores`);
 
 --
 -- Indices de la tabla `patrocinio`
 --
 ALTER TABLE `patrocinio`
   ADD PRIMARY KEY (`idPatrocinio`),
-  ADD KEY `patrocinio` (`idPatrociador`),
+  ADD KEY `patrocinio` (`idSponsor`),
   ADD KEY `jugador` (`idJugador`);
+
+--
+-- Indices de la tabla `sponsor`
+--
+ALTER TABLE `sponsor`
+  ADD PRIMARY KEY (`idSponsor`);
+
+--
+-- Indices de la tabla `torneo`
+--
+ALTER TABLE `torneo`
+  ADD PRIMARY KEY (`idTorneo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `encuentro`
+--
+ALTER TABLE `encuentro`
+  MODIFY `idEncuentro` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `estadio`
+--
+ALTER TABLE `estadio`
+  MODIFY `idEstadio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `jugador`
+--
+ALTER TABLE `jugador`
+  MODIFY `idJugador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `sponsor`
+--
+ALTER TABLE `sponsor`
+  MODIFY `idSponsor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `torneo`
+--
+ALTER TABLE `torneo`
+  MODIFY `idTorneo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
+-- Filtros para la tabla `encuentro`
+--
+ALTER TABLE `encuentro`
+  ADD CONSTRAINT `encuentro_ibfk_1` FOREIGN KEY (`idEncuentro`) REFERENCES `torneo` (`idTorneo`),
+  ADD CONSTRAINT `estadio` FOREIGN KEY (`idEstadio`) REFERENCES `estadio` (`idEstadio`),
+  ADD CONSTRAINT `j1` FOREIGN KEY (`jugador1`) REFERENCES `jugador` (`idJugador`),
+  ADD CONSTRAINT `j2` FOREIGN KEY (`jugador2`) REFERENCES `jugador` (`idJugador`),
+  ADD CONSTRAINT `jGanador` FOREIGN KEY (`jugadorGanador`) REFERENCES `jugador` (`idJugador`);
+
+--
 -- Filtros para la tabla `patrocinio`
 --
 ALTER TABLE `patrocinio`
   ADD CONSTRAINT `jugador` FOREIGN KEY (`idJugador`) REFERENCES `jugador` (`idJugador`),
-  ADD CONSTRAINT `patrocinio` FOREIGN KEY (`idPatrociador`) REFERENCES `sponsor` (`idPatrocinadores`);
+  ADD CONSTRAINT `patrocinio` FOREIGN KEY (`idSponsor`) REFERENCES `sponsor` (`idSponsor`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
